0424f2d79ce71ab9f6002574263714c4cd7ee412
Mateusz <mateuszslotwinski16@gmail.com>
2022-01-18 19:24:25 +0100

    Production mode

--- src/app.go
+++ src/app.go
@@ -3,10 +3,17 @@ package src
 import (
 	Config "GenesisDAT/src/config"
 	Router "GenesisDAT/src/router"
+
+	gin "github.com/gin-gonic/gin"
 )
 
 func App() {
+	if Config.Mode == "PROD" {
+		gin.SetMode(gin.ReleaseMode)
+	}
+
 	App := Router.CreateServer()
+
 	App.Run(Config.Port)
 }
 
